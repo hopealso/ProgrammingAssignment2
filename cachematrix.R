a48ea537b1c35838b252bc3b6833d2370f937533
Hope Foster-Reyes <hopealso@gmail.com>
2015-09-27 10:58:41 -0500

    Change 'm' to 'cache' for clarity, revisions to comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,34 @@
 ## The functions makeCacheMatrix and cacheSolve work together to calculate and cache the inverse of a matrix.
-## If the inverse has already been calculated (and the matrix has not changed), cachesolve retrieves the inverse from the cache.
+## If the inverse has already been calculated (& the matrix has not changed), cachesolve retrieves it from the cache.
 ## A variable with the results makeCacheMatrix must be initialized in cacheSolve's environment before running cacheSolve.
 
 ## makeCacheMatrix produces a "special matrix" which is actually a list containing 4 functions.
+## This "special matrix" holds the cache of any previously calculated inverses.
 
 makeCacheMatrix <- function(x = matrix()) {
-    ## 
-    
-    ## Initialize m as NULL
-    m <- NULL
+    ## Initialize cache as NULL
+    ## When cache is NULL, it's a sign that the inverse hasn't been calculated.
+    cache <- NULL
     
     ###### 
     ## Create a set of 4 functions that this "special matrix" can run
     
-    ## This function of the resultant "special matrix" list of functions allows resetting the matrix
-    ## When doing so, it also resets the value of m in the special matrix, signaling the need to recalculate
-    set <- function(y) {
-        x <<- y
-        m <<- NULL
+    ## This function of the "special matrix" allows resetting the matrix to be acted upon
+    ## When doing so, it also resets the value of cache, signaling the need to recalculate
+    set <- function(newmatrix) {
+        x <<- newmatrix
+        cache <<- NULL
     }
     
-    ## This function simply returns the matrix to be inverted
+    ## This function simply returns the matrix to be acted upon
     get <- function() x
     
     ## This function sets the inverse in the "special matrix" but does not actually calculate it
-    ## It must be passed the inverse by the function that calls it
-    setinverse <- function(inverse) m <<- inverse
+    ## It must be passed the inverse as an argument
+    setinverse <- function(inverse) cache <<- inverse
     
     ## This function returns the inverse currently set 
-    getinverse <- function() m
+    getinverse <- function() cache
     
     ######
     
